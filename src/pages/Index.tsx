af045a0683e5662bacad0c64cd16f2ecca23278e
Ivan <itrushkov77@gmail.com>
2026-05-08 04:31:42 +0300

    Publish Web Studio project 8c235fbc-d6f9-4267-8d06-f5b30a4c197d

---
+++ src/pages/Index.tsx
@@ -0,0 +1,23 @@
+import HeroSection from "@/components/HeroSection";
+import ReviewsSection from "@/components/ReviewsSection";
+import ServicesSection from "@/components/ServicesSection";
+import PortfolioSection from "@/components/PortfolioSection";
+import ProcessSection from "@/components/ProcessSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <main>
+      <HeroSection />
+      <ReviewsSection />
+      <ServicesSection />
+      <PortfolioSection />
+      <ProcessSection />
+      <ContactSection />
+      <Footer />
+    </main>
+  );
+};
+
+export default Index;
